7c788cb1186c9a7207c0f162386488b04cbcbca3
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2025-02-01 21:32:54 +0530

    Refactor address route handler for improved readability and error handling

--- src/app/api/address/route.js
+++ src/app/api/address/route.js
@@ -1,4 +1,3 @@
-
 import dbConnect from "@/app/lib/dbConnect";
 import Address from "@/models/address.model";
 import User from "@/models/userModel";
@@ -10,41 +9,60 @@ export async function POST(req) {
       await req.json();
     if (!street || !city || !state || !postalCode || !country || !phone) {
       return NextResponse.json(
-        { message: "All fields are required"},
+        { message: "All fields are required" },
         { status: 400 }
       );
     }
 
     await dbConnect();
 
-    const address = await Address.create({street, city, state, postalCode, country, phone});
+    const address = await Address.create({
+      street,
+      city,
+      state,
+      postalCode,
+      country,
+      phone,
+    });
 
-    const user = await User.findByIdAndUpdate(user_Id, {
+    const user = await User.findByIdAndUpdate(
+      user_Id,
+      {
         $push: { address: address._id },
-      }, { new: true });
+      },
+      { new: true }
+    );
 
-      if(!user){
-        return NextResponse.json({ message: "User not found"}, { status: 404 });
-      }
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
 
-      return NextResponse.json({ message: "Address added successfully", address });
-
+    return NextResponse.json({
+      message: "Address added successfully",
+      address,
+    });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "An error occurred" }, { status: 500 });
   }
 }
 
-export async function GET(req){
-    const {searchParams} = new URL(req.url);
-    const userId = searchParams.get("userId");
-    if(!userId) {
-        return NextResponse.json({ message: "User ID is required"}, { status: 400 });
-    }
-    const user = await User.findById(userId).populate("address");
-    if(user.address.length === 0){
-        return NextResponse.json({ message: "User does not have any addresses"}, { status: 404 });
-    }
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get("userId");
+  if (!userId) {
+    return NextResponse.json(
+      { message: "User ID is required" },
+      { status: 400 }
+    );
+  }
+  const user = await User.findById(userId).populate("address");
+  if (user.address.length === 0) {
+    return NextResponse.json(
+      { message: "User does not have any addresses" },
+      { status: 404 }
+    );
+  }
 
-    return NextResponse.json({address: user.address}, {status: 200});
+  return NextResponse.json({ address: user.address }, { status: 200 });
 }
