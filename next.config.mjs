ca4db9509d001ea2f8abb89e934e9c527115a511
jaykishan001 <davidjsd90@gmail.com>
2025-02-01 23:14:02 +0530

    image

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
     domains: ['images.unsplash.com', 'loremflickr.com'], // Add the Unsplash and LoremFlickr domains
-=======
-    domains: ["images.unsplash.com", "loremflickr.com"], // Add the Unsplash domain
->>>>>>> 9e01f4679981ceab31a41a1d850c4dd93a684eba
   },
 };
 
