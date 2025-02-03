702796cc12b82bc234f70037d49ae8c640721281
jaykishan001 <davidjsd90@gmail.com>
2025-02-03 22:06:09 +0530

    build error fixed

--- src/app/admin/products/upload/page.jsx
+++ src/app/admin/products/upload/page.jsx
@@ -1,10 +1,13 @@
 import { ProductUploadForm } from "@/components/ProductUploadForm"
+import { Suspense } from "react"
 
 export default function ProductUploadPage() {
   return (
     <div className="container mx-auto py-2">
       <h1 className="text-3xl text-center font-bold mt-20">Upload New Product</h1>
+      <Suspense fallback={<div>Loading...</div>}>
       <ProductUploadForm />
+      </Suspense>
     </div>
   )
 }
